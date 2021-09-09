88d0324d97b05382f6a6f9334f9e73eccfb8fadd
plugo <plugo@americavirtualsa.com>
2021-09-08 22:23:30 -0300

    Configuracion inicial

--- index.js
+++ index.js
@@ -1,31 +1,42 @@
 // const http =  require('http')
-const express = require("express");
+const express = require("express")
 
-const app = express();
+const app = express()
 
-const axios = require("axios");
-const citys = require("./ciudades.json");
+const axios = require("axios")
+const cities = require("./ciudades.json")
 
 app.get("/api/city", (request, response) => {
-  response.json(citys);
-});
+  response.setHeader('Content-Type', 'application/json')
+  response.status(200).send(cities)
+})
 
 app.get("/api/city/:id", (request, response) => {
-  const { id } = request.params;
-
-  response.json(citys.filter((city) => city.id === parseInt(id)));
-});
+  const id = Number(request.params.id)
+  console.log(id)
+  if (!id) {
+    response.status(204).end('error')
+  }
+  else{
+    response.setHeader('Content-Type', 'application/json').status(200).send(cities.filter((city) => city.id === id))
+  }
+})
 
 app.get("/api/GetLocationIp/:ip", async (request, response) => {
+
   const res = await axios.get(
     `http://ip-api.com/json/${request.params.ip}`
-  );
+  )
+
   const { country, regionName, city } = res.data
-  response.send(JSON.stringify({ country, regionName, city }));
-});
+
+  response.setHeader('Content-Type', 'application/json')
+
+  response.send(JSON.stringify({ country, regionName, city }))
+})
 
 
-const PORT = 3001;
+const PORT = 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
---
+++ readme.md
@@ -0,0 +1,1 @@
+Instalar Extension REST client para utilizar las peticiones armadas en la carpeta request
