ca08c1ba6d4af4cc9d23f0e2045fdca4d69f29b6
plugo <plugo@americavirtualsa.com>
2021-09-08 23:04:13 -0300

    configuracion prettier

--- index.js
+++ index.js
@@ -1,40 +1,42 @@
-// const http =  require('http')
-const express = require("express")
+const express = require('express')
+const cors = require('cors')
 
 const app = express()
 
-const axios = require("axios")
-const cities = require("./ciudades.json")
+app.use(cors())
+app.use(express.json())
 
-app.get("/api/city", (request, response) => {
+const axios = require('axios')
+const cities = require('./ciudades.json')
+
+app.get('/api/city', (request, response) => {
   response.setHeader('Content-Type', 'application/json')
   response.status(200).send(cities)
 })
 
-app.get("/api/city/:id", (request, response) => {
+app.get('/api/city/:id', (request, response) => {
   const id = Number(request.params.id)
   console.log(id)
   if (!id) {
     response.status(204).end('error')
-  }
-  else{
-    response.setHeader('Content-Type', 'application/json').status(200).send(cities.filter((city) => city.id === id))
+  } else {
+    response
+      .setHeader('Content-Type', 'application/json')
+      .status(200)
+      .send(cities.filter((city) => city.id === id))
   }
 })
 
-app.get("/api/GetLocationIp/:ip", async (request, response) => {
-
-  const res = await axios.get(
-    `http://ip-api.com/json/${request.params.ip}`
-  )
-
-  const { country, regionName, city } = res.data
-
+app.get('/api/GetLocationIp/:ip', async (request, response) => {
+  const res = await axios.get(`http://ip-api.com/json/${request.params.ip}`)
+  const { regionName, city } = res.data
   response.setHeader('Content-Type', 'application/json')
-
-  response.send(JSON.stringify({ country, regionName, city }))
+  response.send(JSON.stringify({ city: city + ', ' + regionName }))
 })
 
+app.use((request, response) => {
+  response.status(404).json({ error: `EndPoint no encontrado ${request.path}` })
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
