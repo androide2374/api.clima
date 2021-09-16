7caed6ca7744c3fd898dea8ec4bfddac4d209415
plugo <plugo@americavirtualsa.com>
2021-09-15 22:54:06 -0300

    listo

--- controllers/getLocationIp.js
+++ controllers/getLocationIp.js
@@ -3,56 +3,11 @@ const location = express.Router()
 
 const axios = require('axios')
 
-location.get('/location/:ip', async (req, response) => {
-  const res = await axios.get(`http://ip-api.com/json/${req.params.ip}`)
+location.get('/location/', async (req, response) => {
+  const res = await axios.get('http://ip-api.com/json/')
 
   response.setHeader('Content-Type', 'application/json')
   response.send(JSON.stringify(res.data))
 })
-
-// // GetLocationIp.get('/GetWeatherIp/', async (req, res) => {
-// //   const dataip = await axios.get('http://ip-api.com/json/')
-// //   const { lon, lat } = dataip.data
-
-// //   console.log({ lon, lat })
-
-// //   try {
-// //     const weatherdata = await axios.get(
-// //       `http://api.openweathermap.org/data/2.5/find?lat=${lat}&lon=${lon}&cnt=1&appid=${API_KEY_WEATHER}`
-// //     )
-// //     const data = weatherdata.data
-
-// //     const { list } = data
-
-// //     console.log(list)
-
-// //     res.setHeader('Content-Type', 'application/json')
-
-// //     res.status(200).json(data)
-// //   } catch (error) {
-// //     res.status(404)
-// //     console.log(error)
-// //   }
-// // })
-
-// // GetLocationIp.get('/GetWeatherIp/:city', async (req, res) => {
-// //   const { city } = req.params
-
-// //   const weatherdata = await axios
-// //     .get(
-// //       `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY_WEATHER}`
-// //     )
-// //     .catch((error) => res.status(404).json(error))
-// //   if (weatherdata) {
-// //     const { data } = weatherdata
-// //     if (data.cod === 200) {
-// //       res.setHeader('Content-Type', 'application/json')
-// //       res.status(200).json(data)
-// //     }
-// //   }
-// //   // } catch (error) {
-// //   //   res.status(404).end(JSON.stringify(error.message))
-// //   // }
-// })
 
 module.exports = location
--- controllers/weather.js
+++ controllers/weather.js
@@ -1,36 +1,77 @@
 const express = require('express')
 const weather = express.Router()
 
+const ciudades = require('../ciudades.json')
+
 const axios = require('axios')
 const API_KEY_WEATHER =
   process.env.API_KEY || '212b49dc91ea1c61dad9b96abb1c004d'
+const PORT = process.env.PORT || 3001
 
 weather.get('/current/:city?', async (req, res) => {
-  let city = null
-  console.log(req.params.city)
+  let coords = {}
+
   if (req.params.city) {
-    city = req.params.city
+    coords = ciudades[req.params.city]
+    console.log('--current by city param--')
   } else {
-    console.log('Get city ip')
-    const response = await axios.get('http://ip-api.com/json/')
-    city = response.data.city
+    console.log('--current by city ip--')
+    const response = await axios.get(`http://localhost:${PORT}/v1/location`)
+    const { lon, lat } = response.data
+    coords = { lon, lat }
   }
-  await axios
-    .get(
-      `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=es&appid=${API_KEY_WEATHER}`
-    )
-    .then((response) => {
-      if (response.data.cod === 200) {
-        res.setHeader('Content-Type', 'application/json')
-        res.status(200).json(response.data)
-      }
-    })
-    .catch((error) => res.status(404).json(error))
+  // `https://api.openweathermap.org/data/2.5/onecall?lat=${coords.lat}&lon=${coords.lon}&exclude=hourly,minutely&appid=${API_KEY_WEATHER}&units=metric`
+  console.log('--initial current request--')
+
+  const response = await axios.get(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=${API_KEY_WEATHER}&units=metric`
+  )
+
+  if (response.data) {
+    console.log('--current response ok --')
+
+    res.setHeader('Content-Type', 'application/json')
+    res.status(200).json(response.data)
+  } else {
+    console.log('--current response error --')
+
+    res.status(404).json(response)
+  }
 })
 
-// weather.get('/forecast/:city ', (req, res) => {
-//   if (req.params.city) {
-//   }
-// })
+weather.get('/forecast/:city?', async (req, res) => {
+  let coords = {}
+  let scity = ''
+
+  if (req.params.city) {
+    coords = ciudades[req.params.city]
+    scity = req.params.city
+    console.log('--current by city param--')
+  } else {
+    console.log('--current by city ip--')
+    const response = await axios.get(`http://localhost:${PORT}/v1/location`)
+    const { lon, lat, city } = response.data
+    scity = city
+    coords = { lon, lat }
+  }
+  console.log('--initial current request--')
+
+  const response = await axios.get(
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${coords.lat}&lon=${coords.lon}&exclude=hourly,minutely&appid=${API_KEY_WEATHER}&units=metric`
+  )
+
+  if (response.data) {
+    console.log('--current response ok --')
+    const { daily, lat, lon } = response.data
+    const forecast = daily.slice(1, 6)
+
+    res.setHeader('Content-Type', 'application/json')
+    res.status(200).json({ forecast, lat, lon, scity })
+  } else {
+    console.log('--current response error --')
+
+    res.status(404).json(response)
+  }
+})
 
 module.exports = weather
